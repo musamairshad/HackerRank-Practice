8e78aeff222c2f68c449804bd93bd5dc2823b7a0
Muhammad Usama <imusamairshad@gmail.com>
2023-02-06 22:16:16 +0500

    More programs added.

---
+++ Python/16. Numpy/001. Arrays.py
@@ -0,0 +1,9 @@
+# NAME: MUHAMMAD USAMA
+# DIFFICULTY LEVEL: EASY
+# Max Score: 20
+
+import numpy
+
+def arrays(arr):
+    newArray = numpy.array(arr, dtype=float)
+    return newArray[::-1]
---
+++ Python/5. Math/004. Mod Divmod.py
@@ -0,0 +1,9 @@
+# NAME: MUHAMMAD USAMA
+# DIFFICULTY LEVEL: EASY
+# Max Score: 10
+
+a = int(input())
+b = int(input())
+print(a // b)
+print(a % b)
+print(divmod(a, b))
