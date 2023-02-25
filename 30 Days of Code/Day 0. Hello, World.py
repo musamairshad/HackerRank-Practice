6ead15e44ef655e810ff5131775e1838ac575275
Muhammad Usama <imusamairshad@gmail.com>
2023-02-25 17:32:13 +0500

    30 days of Code folder created.

---
+++ 30 Days of Code/Day 0. Hello, World.py
@@ -0,0 +1,7 @@
+# NAME: MUHAMMAD USAMA
+# DIFFICULTY LEVEL: EASY                                              
+# Max Score: 30
+
+input_string = input()
+print('Hello, World.')
+print(input_string)
