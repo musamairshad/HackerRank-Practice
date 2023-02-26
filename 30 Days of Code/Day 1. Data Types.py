42b0b6d0712fac138b02e60d135cba8ce6a86598
Muhammad Usama <imusamairshad@gmail.com>
2023-02-26 18:30:42 +0500

    Day 1 added.

---
+++ 30 Days of Code/Day 1. Data Types.py
@@ -0,0 +1,13 @@
+# NAME: MUHAMMAD USAMA
+# DIFFICULTY LEVEL: EASY                                              
+# Max Score: 30
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+no = int(input())
+doub = float(input())
+sr = input()
+print(i + no)
+print(d + doub)
+print(s + sr)
