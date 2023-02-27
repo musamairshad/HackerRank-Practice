e0a0ff5f6fffae814b2f25743e422cde6b0497d4
Muhammad Usama <imusamairshad@gmail.com>
2023-02-27 12:14:03 +0500

    Weather Observation 15 added.

--- SQL/03. Aggregation/012. Weather Observation Station 15.sql
+++ SQL/03. Aggregation/012. Weather Observation Station 15.sql
@@ -1,3 +1,6 @@
 -- NAME: MUHAMMAD USAMA
 -- DIFFICULTY LEVEL: EASY                                              
 -- Max Score: 15
+
+select round(long_w, 4) from station where lat_n = (select max(lat_n) from station 
+where lat_n < 137.2345);
