a0db45de962e7071c66e5c2d4edb95af6123fe71
Muhammad Usama <imusamairshad@gmail.com>
2023-02-27 12:43:24 +0500

    Average Population of Each Continent modified.

--- SQL/04. Basic Join/003. Average Population of Each Continent.sql
+++ SQL/04. Basic Join/003. Average Population of Each Continent.sql
@@ -2,5 +2,10 @@
 -- DIFFICULTY LEVEL: EASY                                              
 -- Max Score: 10
 
+-- We use group by clause because it was given in the question that query the
+-- continent names with respect to average city population.
+-- We use floor because it was given in the question that avg city population
+-- rounded down to the nearest integer.
+
 select country.continent, floor(avg(city.population)) from country, city where
 country.code = city.countrycode group by country.continent;
