14c763cf2c9f3a8d1d9cc5f0280fc376cd671def
Muhammad Usama <imusamairshad@gmail.com>
2023-02-28 11:20:24 +0500

    Day 3 added.

---
+++ 30 Days of Code/Day 3. Intro to Conditional Statements.py
@@ -0,0 +1,14 @@
+# NAME: MUHAMMAD USAMA
+# DIFFICULTY LEVEL: EASY                                              
+# Max Score: 30
+
+if (__name__) == '__main__':
+    N = int(input().strip())
+    if(N % 2 != 0):
+        print("Weird")
+    elif (N % 2 == 0 and 2 <= N <= 5):
+        print("Not Weird")
+    elif (N % 2 == 0 and 6 <= N <= 20):
+        print("Weird")
+    elif (N % 2 == 0 and N > 20):
+        print("Not Weird")
