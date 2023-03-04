a73f0030f853393fe39c7b9c9d40769eb5620478
Muhammad Usama <imusamairshad@gmail.com>
2023-03-04 16:27:43 +0500

    Day 7 file added.

--- 30 Days of Code/Day 7. Arrays.py
+++ 30 Days of Code/Day 7. Arrays.py
@@ -2,3 +2,8 @@
 # DIFFICULTY LEVEL: EASY                                              
 # Max Score: 30
 
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    for i in reversed(range(len(arr))):
+        print(arr[i],end=" ")
