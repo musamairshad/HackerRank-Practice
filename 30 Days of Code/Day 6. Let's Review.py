9a0ea26195cf1200cb0f2446ba823d93a6ad9009
Muhammad Usama <imusamairshad@gmail.com>
2023-03-04 12:33:52 +0500

    Day 6 added.

--- 30 Days of Code/Day 6. Let's Review.py
+++ 30 Days of Code/Day 6. Let's Review.py
@@ -1,3 +1,39 @@
 # NAME: MUHAMMAD USAMA
 # DIFFICULTY LEVEL: EASY                                              
 # Max Score: 30
+
+
+# 1st Method
+
+# T = int(input())
+# for i in range(T):
+#     S = input()
+#     lst = []
+#     for i in range(len(S)):
+#         if(i % 2 == 0):
+#             lst.append(S[i])
+#             s = "".join(lst)
+#     print(s,end=" ")
+#     lst = []
+#     for i in range(len(S)):
+#         if(i % 2 != 0):
+#             lst.append(S[i])
+#             s = "".join(lst)
+#     print(s)
+
+
+
+
+# 2nd Method
+
+T = int(input())
+for i in range(T):
+    S = input()
+    evenCharacters = ""
+    oddCharacters = ""
+    for i in range(len(S)):
+        if(i % 2 == 0):
+            evenCharacters += S[i]
+        else:
+            oddCharacters += S[i]
+    print(evenCharacters,oddCharacters)  
