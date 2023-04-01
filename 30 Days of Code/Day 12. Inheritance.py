0e5398e3ed9e95887517ebb88144ed47339b9d69
Muhammad Usama <imusamairshad@gmail.com>
2023-04-01 14:39:29 +0500

    30 Days of Code/Day 12 and Day 8 modified.

--- 30 Days of Code/Day 12. Inheritance.py
+++ 30 Days of Code/Day 12. Inheritance.py
@@ -1,3 +1,11 @@
 # NAME: MUHAMMAD USAMA
 # DIFFICULTY LEVEL: EASY                                              
 # Max Score: 30
+
+class Person:
+    def __init__(self, firstName, lastName, idNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
+    
+    
--- 30 Days of Code/Day 8. Dictionaries and Maps.py
+++ 30 Days of Code/Day 8. Dictionaries and Maps.py
@@ -1,3 +1,10 @@
 # NAME: MUHAMMAD USAMA
 # DIFFICULTY LEVEL: EASY                                              
 # Max Score: 30
+
+n = int(input())
+phoneBook = {}
+for i in range(n):
+    name, phoneNo = input().split()
+    phoneBook.update(m)
+print(phoneBook)
