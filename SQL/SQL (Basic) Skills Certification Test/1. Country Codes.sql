30acf62695122fc5f8ca14c7060c7bff5bd7d713
Muhammad Usama <imusamairshad@gmail.com>
2023-08-03 17:01:15 +0500

    1. Country Codes.sql file is modified.

--- SQL/SQL (Basic) Skills Certification Test/1. Country Codes.sql
+++ SQL/SQL (Basic) Skills Certification Test/1. Country Codes.sql
@@ -1,1 +1,5 @@
-select customers.customer_id, customers.name, concat('+', country_codes.country_code, customers.phone_number)  from customers, country_codes where customers.country = country_codes.country order by customers.customer_id;
+-- NAME: MUHAMMAD USAMA
+
+select customers.customer_id, customers.name, 
+concat('+', country_codes.country_code, customers.phone_number) from customers, country_codes
+where customers.country = country_codes.country order by customers.customer_id;
