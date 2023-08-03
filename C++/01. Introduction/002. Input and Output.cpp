ac67a5d4ab69f175772ff39ef299f7a0736ecdb2
Muhammad Usama <imusamairshad@gmail.com>
2023-08-03 17:03:27 +0500

    Input and Output.cpp is added.

---
+++ C++/01. Introduction/002. Input and Output.cpp
@@ -0,0 +1,16 @@
+/* NAME: MUHAMMAD USAMA
+   DIFFICULTY LEVEL: EASY 
+   Max Score: 5
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {  
+    int a;
+    int b;
+    int c;
+    cin >> a >> b >> c;
+    cout  << a + b + c;
+    return 0;
+}
