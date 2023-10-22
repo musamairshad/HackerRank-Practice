04aafc9dedb8f31976f98440f1ba252228c1a0bd
Muhammad Usama <imusamairshad@gmail.com>
2023-10-22 15:59:53 +0500

    001. Structs.cpp is added.

---
+++ C++/03. Classes/001. Structs.cpp
@@ -0,0 +1,29 @@
+/* NAME: MUHAMMAD USAMA
+   DIFFICULTY LEVEL: EASY
+   Max Score: 10
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct Student
+{
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+};
+
+int main()
+{
+    Student st;
+
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+
+    return 0;
+}
